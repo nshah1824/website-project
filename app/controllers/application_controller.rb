e8aafead74dc67d9e241f050aeffa03d1279a19c
Nirali <nshah1824@gmail.com>
2017-11-14 19:35:39 +0000

    Final Lab 10

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,16 @@ class ApplicationController < ActionController::Base
   
   protect_from_forgery with: :exception
   
+  def current_cart
+  Cart.find(session[:cart_id])
+  rescue ActiveRecord::RecordNotFound
+    cart=Cart.create
+    session[:card_id]=card.id
+    cart
+  end
+  
+  helper_method :current_cart
+  
   protected
   
   def authorize
